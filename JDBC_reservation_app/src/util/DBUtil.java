d96cdc753357bccf9caee5c26644064ac9a6dfa1
규빈 <127824570+kyubinjin@users.noreply.github.com>
2025-06-07 20:29:54 +0900

    fix: DBUtil url 수정

--- JDBC_reservation_app/src/util/DBUtil.java
+++ JDBC_reservation_app/src/util/DBUtil.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/photo_db?serverTimezone=Asia/Seoul";
+    private static final String URL = "jdbc:mysql://localhost:3306/photodb?serverTimezone=Asia/Seoul";
     private static final String USER = "testuser";
     private static final String PASSWORD = "testpw"; 
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // ✅ 드라이버 명시적으로 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println("MySQL JDBC 드라이버를 찾을 수 없습니다.");
             e.printStackTrace();
